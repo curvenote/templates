7b23bc19702ec96716bbd4c532dbc5f0b51ca172
Steve Purves <stevejpurves@gmail.com>
2021-06-06 20:49:39 +0100

    switch to python logic

--- .github/actions/process-templates-action/entrypoint.sh
+++ .github/actions/process-templates-action/entrypoint.sh
@@ -1,4 +1,4 @@
-
-# https://github.community/t/how-can-i-access-the-current-repo-context-and-files-from-a-docker-container-action/17711
-ls /github/workspace
+#!/bin/bash
+echo "Repository listing:"
+ls -l /github/workspace
 cd /github/workspace
